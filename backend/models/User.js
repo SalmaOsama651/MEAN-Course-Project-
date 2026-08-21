049d6b555bb88ab733a1c909de6e5f654820d7ad
SalmaOsama651 <salmaosama651@gmail.com>
2026-08-21 21:42:05 +0300

    Complete authentication and user profile

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["player", "admin"],
+        default: "player"
+    },
+
+    totalScore: {
+        type: Number,
+        default: 0
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
