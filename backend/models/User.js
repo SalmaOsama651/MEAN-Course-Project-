bded9ee16b9bb5d21fec0a9b04356c5fcb14614b
SalmaOsama651 <salmaosama651@gmail.com>
2026-08-22 12:02:48 +0300

    Connect APIs with MongoDB

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,41 +1,41 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 3
-    },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+  },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
 
-    password: {
-        type: String,
-        required: true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    role: {
-        type: String,
-        enum: ["player", "admin"],
-        default: "player"
-    },
+  role: {
+    type: String,
+    enum: ["player", "admin"],
+    default: "player",
+  },
 
-    totalScore: {
-        type: Number,
-        default: 0
-    },
+  totalScore: {
+    type: Number,
+    default: 0,
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Users", userSchema);
