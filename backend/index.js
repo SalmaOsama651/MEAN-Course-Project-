3f2f6fda5fb5731bdf00edad0001a57cd95faff4
SalmaOsama651 <salmaosama651@gmail.com>
2026-08-25 02:21:10 +0300

    Shahd's backend finally

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,6 @@
-require("dotenv").config();
-
 const express = require("express");
-const connectDB = require("./config/db");
-
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db.config.js");
+const caseRoutes = require("./routes/cases.routes.js");
 
 const app = express();
 
@@ -12,16 +8,9 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api", caseRoutes);
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "API is running",
-  });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
