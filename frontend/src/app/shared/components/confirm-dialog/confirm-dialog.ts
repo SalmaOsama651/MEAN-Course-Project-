b987b18b9902f45242f371c3e671a12992d8bceb
SalmaOsama651 <salmaosama651@gmail.com>
2026-08-27 12:50:43 +0300

    Add shared components

---
+++ frontend/src/app/shared/components/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  templateUrl: './confirm-dialog.html',
+})
+export class ConfirmDialogComponent {
+  @Input() title = 'Confirm Action';
+  @Input() message = 'Are you sure you want to continue?';
+  @Input() isOpen = false;
+
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  onConfirm(): void {
+    this.confirm.emit();
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+  }
+}
---
+++ frontend/src/app/shared/components/pagination/pagination.ts
@@ -0,0 +1,44 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.html',
+  styleUrls: ['./pagination.css']
+})
+export class PaginationComponent {
+
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+
+  @Output() pageChange = new EventEmitter<number>();
+
+  goToPage(page: number): void {
+    if (
+      page < 1 ||
+      page > this.totalPages ||
+      page === this.currentPage
+    ) {
+      return;
+    }
+
+    this.pageChange.emit(page);
+  }
+
+  previousPage(): void {
+    this.goToPage(this.currentPage - 1);
+  }
+
+  nextPage(): void {
+    this.goToPage(this.currentPage + 1);
+  }
+
+  get pages(): number[] {
+    return Array.from(
+      { length: this.totalPages },
+      (_, index) => index + 1
+    );
+  }
+}
